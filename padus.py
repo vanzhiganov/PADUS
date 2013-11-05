4ca5e8fc6651a26a35ab250bff5ee5b30122f073
vanzhiganov <vanzhiganov@ya.ru>
2013-11-05 21:28:37 +0400

    udpate

--- padus.py
+++ padus.py
@@ -10,7 +10,40 @@ ldapsearch -x -H ldap://172.16.2.201 -D cn=ldap,cn=users,dc=gm,dc=local -w ldap 
 import os
 import sys
 import pwd
+import grp
 import ldap
+
+def findgroup(group_id):
+    # Load all of the user data, sorted by username
+    all_groups = grp.getgrall()
+    interesting_groups = sorted((g for g in all_groups if not g.gr_name.startswith('_')), key=operator.attrgetter('gr_name'))
+    
+    # Print the data
+    for g in interesting_groups:
+		if g.gr_gid == group_id
+            print fmt % (name_length, g.gr_name, g.gr_gid, g.gr_passwd, ', '.join(g.gr_mem))
+            return true
+            
+    return false
+    
+# Find the longest length for the name
+"""name_length = max(len(g.gr_name) for g in interesting_groups) + 1
+
+# Print report headers
+fmt = '%-*s %4s %10s %s'
+print fmt % (name_length, 'Name', 
+             'GID', 
+             'Password',
+             'Members')
+print '-' * name_length, '----', '-' * 10, '-' * 30
+
+# Print the data
+for g in interesting_groups:
+    print fmt % (name_length, g.gr_name, 
+                 g.gr_gid, 
+                 g.gr_passwd,
+                 ', '.join(g.gr_mem))
+"""
 
 def finduser(user):
 	try:
